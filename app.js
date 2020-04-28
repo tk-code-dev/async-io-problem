29da7d6478dd1e6f397766d1f0780b698db7e76c
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2020-04-28 21:23:02 +0000

    add Sync and Async appendfile

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ for (let count = 0; count < 500; count++) {
   fs.appendFile(fileName, 'お', 'utf8', () => {});
   fs.appendFile(fileName, '\n', 'utf8', () => {});
 }
+const fileNameSync = './testSync.txt';
+for (let count = 0; count < 500; count++) {
+  fs.appendFileSync(fileNameSync, 'あ', 'utf8');
+  fs.appendFileSync(fileNameSync, 'い', 'utf8');
+  fs.appendFileSync(fileNameSync, 'う', 'utf8');
+  fs.appendFileSync(fileNameSync, 'え', 'utf8');
+  fs.appendFileSync(fileNameSync, 'お', 'utf8');
+  fs.appendFileSync(fileNameSync, '\n', 'utf8');
+}
